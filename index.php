ccd924d0ccced9978e6281bb100593c756bf4fa8
agoudbg <agoudbg@gmail.com>
2023-03-06 10:05:57 +0800

    修复了 title 写成了 placeholder 的问题 😅

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ echo_cr();
                     </div>
                     <div class="detailArea">
                         <div class="cardHeader">
-                            <button class="close" placeholder="关闭" onclick="closeCard('site')">
+                            <button class="close" title="关闭" alt="关闭" onclick="closeCard('site')">
                                 <svg class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M544.448 499.2l284.576-284.576a32 32 0 0 0-45.248-45.248L499.2 453.952 214.624 169.376a32 32 0 0 0-45.248 45.248l284.576 284.576-284.576 284.576a32 32 0 0 0 45.248 45.248l284.576-284.576 284.576 284.576a31.904 31.904 0 0 0 45.248 0 32 32 0 0 0 0-45.248L544.448 499.2z"></path>
                                 </svg>
@@ -134,7 +134,7 @@ echo_cr();
                     </div>
                     <div class="detailArea">
                         <div class="cardHeader">
-                            <button class="close" placeholder="关闭" onclick="closeCard('blog')">
+                            <button class="close" title="关闭" alt="关闭" onclick="closeCard('blog')">
                                 <svg class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M544.448 499.2l284.576-284.576a32 32 0 0 0-45.248-45.248L499.2 453.952 214.624 169.376a32 32 0 0 0-45.248 45.248l284.576 284.576-284.576 284.576a32 32 0 0 0 45.248 45.248l284.576-284.576 284.576 284.576a31.904 31.904 0 0 0 45.248 0 32 32 0 0 0 0-45.248L544.448 499.2z"></path>
                                 </svg>
@@ -183,7 +183,7 @@ echo_cr();
                     </div>
                     <div class="detailArea">
                         <div class="cardHeader">
-                            <button class="close" placeholder="关闭" onclick="closeCard('about')">
+                            <button class="close" title="关闭" alt="关闭" onclick="closeCard('about')">
                                 <svg class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M544.448 499.2l284.576-284.576a32 32 0 0 0-45.248-45.248L499.2 453.952 214.624 169.376a32 32 0 0 0-45.248 45.248l284.576 284.576-284.576 284.576a32 32 0 0 0 45.248 45.248l284.576-284.576 284.576 284.576a31.904 31.904 0 0 0 45.248 0 32 32 0 0 0 0-45.248L544.448 499.2z"></path>
                                 </svg>
@@ -253,7 +253,7 @@ echo_cr();
                     </div>
                     <div class="detailArea">
                         <div class="cardHeader">
-                            <button class="close" placeholder="关闭" onclick="closeCard('friends')">
+                            <button class="close" title="关闭" alt="关闭" onclick="closeCard('friends')">
                                 <svg class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M544.448 499.2l284.576-284.576a32 32 0 0 0-45.248-45.248L499.2 453.952 214.624 169.376a32 32 0 0 0-45.248 45.248l284.576 284.576-284.576 284.576a32 32 0 0 0 45.248 45.248l284.576-284.576 284.576 284.576a31.904 31.904 0 0 0 45.248 0 32 32 0 0 0 0-45.248L544.448 499.2z"></path>
                                 </svg>
