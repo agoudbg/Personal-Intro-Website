bd74064c4edaf76dff805b0f4a1164b32682cddb
agoudbg <agoudbg@gmail.com>
2026-01-31 11:48:20 +0800

    feat: change header style

--- layouts/detail.vue
+++ layouts/detail.vue
@@ -73,7 +73,7 @@ onBeforeUnmount(() => {
     top: 0;
     left: 0;
     width: 100%;
-    height: 50px;
+    height: 60px;
     display: flex;
     justify-content: flex-end;
     align-items: center;
