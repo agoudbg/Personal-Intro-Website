8ec21940b667824ebbb46062bca968d4d6368001
agoudbg <agoudbg@gmail.com>
2026-07-29 21:55:55 +0800

    feat: add friend link exchange contact

--- pages/index/friends.vue
+++ pages/index/friends.vue
@@ -6,6 +6,9 @@
           :dark-icon-url="friend.darkIconUrl" :name="friend.name" :description="friend.description" :host="friend.host"
           :link-action-text="friend.linkActionText" :link="friend.link" />
       </div>
+      <p class="exchange-note">
+        若要交换友链，请发送邮件到 <a href="mailto:i@agou.im">i@agou.im</a>。
+      </p>
     </div>
   </NuxtLayout>
 </template>
@@ -19,8 +22,10 @@ import { friends } from '~/assets/friends';
 .content {
   width: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: stretch;
+  gap: 16px;
   padding: 0px 10px 50px;
 
   .friends-list {
@@ -33,6 +38,17 @@ import { friends } from '~/assets/friends';
 
     .item {
       width: 100%;
+    }
+  }
+
+  .exchange-note {
+    padding: 0 10px;
+    color: var(--color-text-secondary);
+    font-size: 14px;
+    line-height: 1.6;
+
+    a {
+      color: var(--color-accent);
     }
   }
 }
